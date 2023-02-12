d8c1cc984072fb9242854026546fe7f9b22a4edd
Katarzyna Wasilewska <kwasilewska2014@gmail.com>
2023-02-12 18:02:40 +0100

    add changes

--- src/authors/authors.controller.ts
+++ src/authors/authors.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { AuthorsService } from './authors.service';
 
 @Controller('authors')
-export class AuthorsController {}
+export class AuthorsController {
+  constructor(private authorsService: AuthorsService) {}
+
+  @Get('/')
+  getAll() {
+    return this.authorsService.getAll();
+  }
+}
