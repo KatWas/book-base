f50a26da9ed51e95215c66de378f85a00768d90d
Katarzyna Wasilewska <kwasilewska2014@gmail.com>
2023-02-12 19:14:05 +0100

    add changes

--- src/auth-module/auth-module.service.ts
+++ src/auth-module/auth-module.service.ts
@@ -1,7 +1,47 @@
-import { Injectable } from '@nestjs/common';
-import { UsersService } from 'src/users/users.service';
-
+import { Injectable, Post, Body } from '@nestjs/common';
+import { UsersModuleService } from 'src/users-module/users-module.service';
+import { RegisterDTO } from './dtos/register.dto';
+import * as bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 @Injectable()
-export class AuthService {
-  constructor(private usersService: UsersService) {}
+export class AuthModuleService {
+  constructor(
+    private userService: UsersModuleService,
+    private jwtService: JwtService,
+    private configService: ConfigService,
+  ) {}
+
+  public async register(registrationData: RegisterDTO) {
+    const hashedPassword = await bcrypt.hash(registrationData.password, 10);
+    const userData = {
+      email: registrationData.email,
+    };
+    return this.userService.create(userData, hashedPassword);
+  }
+
+  public async validateUser(email: string, password: string) {
+    const user = await this.userService.getByEmail(email);
+    if (
+      user &&
+      (await bcrypt.compare(password, user.password.hashedPassword))
+    ) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+
+  public async createSession(user: any) {
+    const payload = { email: user.email, sub: user.id };
+
+    const accessToken = this.jwtService.sign(payload, {
+      secret: this.configService.get('jwt.secret'),
+      expiresIn: this.configService.get('expiresIn'),
+    });
+
+    return {
+      access_token: accessToken,
+    };
+  }
 }
